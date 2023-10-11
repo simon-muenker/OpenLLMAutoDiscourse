c8c9cb4d876cd5ceba9c623134a23e43794fbd11
Simon Münker <56649035+smnmnkr@users.noreply.github.com>
2023-10-11 19:55:01 +0200

    make config downloadable

--- src/views/Information.vue
+++ src/views/Information.vue
@@ -10,11 +10,23 @@
 
   <h2 class="text-xl sm:text-2xl font-bold tracking-tight text-slate-800 mt-4 mb-2">
     Prompting Configuration
+    <span
+        class="cursor-pointer text-lg font-light text-slate-500"
+        @click="download_json(inference_setup, 'setup')"
+    >
+      (download)
+    </span>
   </h2>
   <vue-json-pretty :data="inference_setup"/>
 
   <h2 class="text-xl sm:text-2xl font-bold tracking-tight text-slate-800 mt-4 mb-2">
     Agent Configuration
+    <span
+        class="cursor-pointer text-lg font-light text-slate-500"
+        @click="download_json(get_agents(), 'agents')"
+    >
+      (download)
+    </span>
   </h2>
   <vue-json-pretty :data="get_agents()"/>
 
@@ -27,4 +39,5 @@ import Header from "@/components/atoms/Header.vue"
 
 import get_agents from "@/api/agents"
 import {inference_setup} from "@/api/inference"
+import {download_json} from "@/common";
 </script>
