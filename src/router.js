1d8a0f70c96991c2bcda7ded1b9513cd4abd9464
Münker, Simon <muenker@uni-trier.de>
2023-10-20 16:57:15 +0200

    scroll top on route change

--- src/router.js
+++ src/router.js
@@ -4,23 +4,27 @@ import HomeView from "@/views/Home.vue"
 import InformationView from "@/views/Information.vue"
 
 export default createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: "/",
-            name: "home",
-            component: HomeView,
-            meta: {
-                title: "ReplyRift: AI Agent Conversation",
+        history: createWebHistory(import.meta.env.BASE_URL),
+        routes: [
+            {
+                path: "/",
+                name: "home",
+                component: HomeView,
+                meta: {
+                    title: "ReplyRift: AI Agent Conversation",
+                },
             },
-        },
-        {
-            path: "/information",
-            name: "information",
-            component: InformationView,
-            meta: {
-                title: "ReplyRift: More Information",
+            {
+                path: "/information",
+                name: "information",
+                component: InformationView,
+                meta: {
+                    title: "ReplyRift: More Information",
+                },
             },
-        },
-    ],
-})
+        ],
+        scrollBehavior(to, from, savedPosition) {
+            return {top: 0}
+        }
+    }
+)
