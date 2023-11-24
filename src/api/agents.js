b8d66c566ce0e71137e3832692f5846d33ada2d5
Münker, Simon <muenker@uni-trier.de>
2023-11-24 09:52:03 +0100

    restructure agents: you description, split name and icon

--- src/api/agents.js
+++ src/api/agents.js
@@ -1,29 +1,35 @@
 export default function getDefaultAgents() {
     return [
         {
-            "name": "plain_model",
-            "label": "\uD83E\uDD16 Plain Model",
-            "persona": ""
+            "id": "plain_model",
+            "name": "Plain Model",
+            "icon": "\uD83E\uDD16",
+            "persona": "You are a neutral Language Model."
         }, {
-            "name": "agreeing_agent",
-            "label": "\uD83D\uDE46 Agreeing Agent",
-            "persona": "while imagining you are an individual agreeing with the content"
+            "id": "agreeing_agent",
+            "name": "Agreeing Agent",
+            "icon": "\uD83D\uDE46",
+            "persona": "You agree with all opinions and seek confirmation."
         }, {
-            "name": "disagreeing_agent",
-            "label": "\uD83D\uDE45 Disagreeing Agent",
-            "persona": "while imagining you are an individual disagreeing with the content"
+            "id": "disagreeing_agent",
+            "name": "Disagreeing Agent",
+            "icon": "\uD83D\uDE45",
+            "persona": "You disagree with all opinions and attack those with opposing opinions."
         }, {
-            "name": "conspiracy_theorist",
-            "label": "\uD83D\uDEF8 Conspiracy Theorist",
-            "persona": "while imagining you are a conspiracy theorist"
+            "id": "conspiracy_theorist",
+            "name": "Conspiracy Theorist",
+            "icon": "\uD83D\uDEF8",
+            "persona": "You are a conspiracy theorist."
         }, {
-            "name": "nobel_prize_winner",
-            "label": "\uD83C\uDFC5 Nobel Prize Winner",
-            "persona": "while imagining you are a nobel prize winner"
+            "id": "nobel_prize_winner",
+            "name": "Nobel Prize Winner",
+            "icon": "\uD83C\uDFC5",
+            "persona": "You are a nobel prize winner."
         }, {
-            "name": "us_congress_member",
-            "label": "\uD83C\uDFDB\uFE0F US Congress Member",
-            "persona": "while imagining you are a member of the U.S. congress"
+            "id": "us_congress_member",
+            "name": "US Congress Member",
+            "icon": "\uD83C\uDFDB\uFE0F",
+            "persona": "You are a member of the U.S. congress."
         }
     ]
 }
