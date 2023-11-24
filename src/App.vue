2fec5531c4730b39fedb1b53d1803c2ebf967019
Münker, Simon <muenker@uni-trier.de>
2023-11-24 16:46:00 +0100

    add global page navigation

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="my-8">
+  <div class="my-8 pb-20">
     <Container>
       <RouterView/>
       <PageFooter/>
     </Container>
   </div>
+  <PageNavigation />
 </template>
 
 <script setup>
 import {RouterView} from "vue-router"
 import Container from "@/components/atoms/Container.vue"
 import PageFooter from "@/components/PageFooter.vue"
+import PageNavigation from "@/components/PageNavigation.vue"
 </script>
---
+++ src/components/PageNavigation.vue
@@ -0,0 +1,53 @@
+<template>
+  <div
+      class="container mx-auto flex flex-row items-center justify-around gap-1 fixed bottom-0 left-0 right-0 w-full bg-rose-600 text-rose-100 pt-1.5 sm:pt-3 pb-1 sm:pb-2 px-12 sm:px-8 rounded-t-full"
+  >
+    <template v-for="link in nav_items">
+      <RouterLink
+          :to=link.route
+          class="relative flex h-full basis-full cursor-pointer flex-col items-center justify-center"
+      >
+        <component :is="link.icon" class="h-6 w-6 sm:h-8 sm:w-8"></component>
+        <span class="text-xs sm:text-sm font-light">
+          {{ link.label }}
+        </span>
+      </RouterLink>
+    </template>
+  </div>
+</template>
+
+<script>
+import Container from "@/components/atoms/Container.vue"
+import {Bars3BottomRightIcon, ChatBubbleLeftRightIcon} from "@heroicons/vue/24/outline"
+import {AdjustmentsVerticalIcon} from "@heroicons/vue/24/solid"
+
+export default {
+  components: {
+    Bars3BottomRightIcon,
+    AdjustmentsVerticalIcon,
+    ChatBubbleLeftRightIcon,
+    Container
+  },
+  data() {
+    return {
+      nav_items: [
+        {
+          route: {name: 'interact'},
+          label: 'Interact',
+          icon: ChatBubbleLeftRightIcon
+        },
+        {
+          route: {name: 'customize'},
+          label: 'Customize',
+          icon: AdjustmentsVerticalIcon
+        },
+        {
+          route: {name: 'more'},
+          label: 'More',
+          icon: Bars3BottomRightIcon
+        }
+      ]
+    }
+  }
+}
+</script>
