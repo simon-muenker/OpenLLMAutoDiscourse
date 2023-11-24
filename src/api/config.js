dc4d3acf480e7f5037627a431a908b6657287f17
Münker, Simon <muenker@uni-trier.de>
2023-11-24 16:45:17 +0100

    update base prompt

--- src/api/config.js
+++ src/api/config.js
@@ -1,2 +1,11 @@
 export const getDefaultModel = () => ("tiiuae/falcon-7b-instruct")
-export const getDefaultPrompt = () => ("Reply in the form of a tweet to the following text {persona}: '''{text}'''")
+export const getDefaultPrompt = () => (`
+I want you to act as a social media user. You will engage in political and social discussions using an informal tone, slang words, and brief sentences.
+
+{persona}
+
+Reply to the following post while considering your history and character. Your response must not exceed 255 characters: 
+"""{text}"""
+
+Response:
+`.trim())
