8d3a3d7de48396a4816aeec5a34b470686364f48
Münker, Simon <muenker@uni-trier.de>
2023-11-24 16:45:34 +0100

    adapt twitter buton style

--- src/components/atoms/Button.vue
+++ src/components/atoms/Button.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-      class="font-medium sm:text-lg text-white px-6 py-3 rounded-lg bg-green-500 hover:opacity-60 disabled:opacity-25 disabled:cursor-not-allowed transition-opacity"
+      class="font-medium sm:text-lg text-white px-6 py-3 rounded-full bg-sky-500 hover:opacity-60 disabled:opacity-25 disabled:cursor-not-allowed transition-opacity"
   >
     <slot></slot>
   </button>
