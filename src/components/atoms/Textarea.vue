6408fa2f9dbe2ef67b466f81c76a259f57aaed06
Münker, Simon <muenker@uni-trier.de>
2023-11-26 12:20:23 +0100

    auto resize text area

--- src/components/atoms/Textarea.vue
+++ src/components/atoms/Textarea.vue
@@ -1,10 +1,10 @@
 <template>
   <textarea
+      ref="self"
       v-model="value"
       :placeholder="placeholder"
-      :rows="rows"
-      class="grow p-2.5 text-slate-700 bg-slate-100 rounded-md border-0"
-      @input="$emit('update', value)"
+      class="grow p-2.5 text-slate-700 bg-slate-100 rounded-md border-0 resize-none"
+      @input="update"
   >
   </textarea>
 </template>
@@ -12,14 +12,17 @@
 <script>
 export default {
   props: {
-    placeholder: String,
-    rows: {
-      type: String,
-      default: "2"
-    },
     defaultValue: {
       type: String,
       default: ""
+    },
+    placeholder: {
+      type: String,
+      default: ""
+    },
+    initialHeight: {
+      type: Number,
+      default: 72
     }
   },
   data() {
@@ -27,7 +30,18 @@ export default {
       value: this.defaultValue
     }
   },
+  mounted() {
+    this.resize()
+  },
   methods: {
+    update() {
+      this.resize()
+      this.$emit('update', this.value)
+    },
+    resize() {
+      this.$refs["self"].style.height = this.initialHeight + "px"
+      this.$refs["self"].style.height = this.$refs["self"].scrollHeight + "px"
+    },
     reset() {
       this.value = this.defaultValue
     }
