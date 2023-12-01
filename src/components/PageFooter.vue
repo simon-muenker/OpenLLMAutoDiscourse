3baacbe1119cf8d4761ef18ddba0df83bec0e309
Münker, Simon <muenker@uni-trier.de>
2023-12-01 15:33:07 +0100

    add git link to footer

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -5,8 +5,13 @@
     <a href="https://www.twon-project.eu/" target="_blank">
       TWON - TWin of Online Social Networks
     </a>
+    <a class="text-slate-600" href="https://github.com/smnmnkr/TWON-Threads" target="_blank">
+      Github
+    </a>
     <a href="https://www.linkedin.com/in/simon-muenker" target="_blank">
       Simon Münker, University of Trier
     </a>
   </div>
 </template>
+<script setup>
+</script>
